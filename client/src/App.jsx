13cb50630f1ac4aa363bac870cc00d3a354f0c94
prasad2206 <60003248+prasad2206@users.noreply.github.com>
2025-09-15 13:44:23 +0530

    force: add client folder for frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SearchBar from "./components/Searchbar";
+
+export default function App() {
+  return (
+    <div className="app-container">
+      <h1 className="title">Autosuggest Search Bar</h1>
+      <SearchBar placeholder="Search technologies, topics..." />
+    </div>
+  );
+}
